d7b8a60d53c2552e63f483a481546ad8e0704d18
Billy Roberts <williamrobertsjr@gmail.com>
2018-09-28 22:38:27 -0400

    Additional variable definitions for table

--- designs.js
+++ designs.js
@@ -1,11 +1,17 @@
 // Select color input
 const color = document.queryselector('#colorPicker');
+
 // Select size input
 const size = document.queryselector('#sizePicker');
+const row = document.queryselector('#inputHeight').value;
+const col = document.queryselector('#inputWidth').value;
+const table = document.queryselector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
+
 function makeGrid() {
+
 
 // Your code goes here!
 
