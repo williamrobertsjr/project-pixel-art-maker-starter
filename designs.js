65ce7b194120bc73941f3b710ff57fe462a34e1b
Billy Roberts <williamrobertsjr@gmail.com>
2018-09-29 17:09:44 -0400

    Add makeGrid functionality when user clicks submit

--- designs.js
+++ designs.js
@@ -6,12 +6,23 @@ const col = document.querySelector('#inputWidth');
 const table = document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-
 size.addEventListener('submit', function(click){
   click.preventDefault();
+  table.innerHTML = "";
   makeGrid();
 }, false);
 
+// makeGrid function should build Pixel Canvas
 function makeGrid() {
-    
+  const table = document.querySelector('#pixelCanvas');
+
+  for (var x = 1; x <= row.value; x++) {
+    const newRow = document.createElement('tr');
+    table.appendChild(newRow);
+
+    for (var y = 1; y <= col.value; y++) {
+      const newCol = document.createElement('td');
+      newRow.appendChild(newCol);
+    }
+  }
 }
