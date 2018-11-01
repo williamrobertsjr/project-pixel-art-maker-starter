a117103da50f1d3f670afe6cd8ec8ac3cf75e0c9
Billy Roberts <williamrobertsjr@gmail.com>
2018-11-01 00:07:10 -0400

    Added reset button, toggle cell color when clicked

--- designs.js
+++ designs.js
@@ -4,12 +4,20 @@ const size = document.querySelector('#sizePicker');
 const row = document.querySelector('#inputHeight');
 const col = document.querySelector('#inputWidth');
 const table = document.querySelector('#pixelCanvas');
+const reset = document.querySelector('#resetGrid');
 
 // When size is submitted by the user, call makeGrid()
 size.addEventListener('submit', function(click){
   click.preventDefault();
   table.innerHTML = "";
   makeGrid();
+}, false);
+
+// Clear table and reset grid size values when reset button is clicked
+reset.addEventListener('click', function(click){
+  console.log('you cliked reset')
+  size.reset();
+  table.innerHTML = "";
 }, false);
 
 // makeGrid function should build Pixel Canvas
@@ -26,3 +34,20 @@ function makeGrid() {
     }
   }
 }
+
+
+// change pixel canvas color when clicked to color chosen in color picker
+table.addEventListener ('mousedown', function (td, event) {
+    console.log(td.target);
+
+    // if cell is white - change to color value
+    if (td.target.style.backgroundColor == ""){
+      console.log('background is initial');
+      td.target.style.backgroundColor = color.value;
+  }
+  // if cell has a color value change it back to white
+    else {
+      td.target.style.backgroundColor = "";
+  }
+
+})
